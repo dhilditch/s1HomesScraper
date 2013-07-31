e714778977b70fa5a9a8bef0d361f8c53d001cbe
AliMillican1 <alastair.millican@hotmail.co.uk>
2013-07-31 14:48:43 +0100

    Working scraper! Saved in file s1.hs
    Hallelujah.

---
+++ s1.js
@@ -0,0 +1,47 @@
+	
+    var request = require('request'); // lets you connect to web pages
+
+	var cheerio = require('cheerio'); // cheerio mimics the DOM and jQuery/CSS style selectors
+
+
+	
+
+
+	for (counter=1;counter<3;counter++) {
+		var url = 'http://www.s1homes.com/property-for-sale/forsale_search_results.cgi?type=House&newhomes=yes&sort=da&page=' +counter;
+
+		request(url, {proxy: 'http://localhost:8888'}, function(err, resp, body) {
+
+			if (err)
+
+				throw err;
+
+			$ = cheerio.load(body);
+
+			$('.imgPlaceholder a:contains()').each(function() {
+
+				console.log ($(this).attr('href'));
+
+				request ('http://www.s1homes.com/' + $(this).attr('href'), function(err,resp,body) {
+					//looks for meta tag with property og:url and takes the url that is assigned to content and then prints it out 
+					$ = cheerio.load(body);
+					//scrapeid = $('meta[name="description"]').attr('content');
+					//console.log ('ID:' + $('a[class="schedule_download"]').attr('href'));
+
+					console.log ('Address:' + $('.detailsKeyPropertyDetails h1').text());
+					//console.log ('Rooms:' + $('.detailsKeyPropertyDetails h2').text());
+					//console.log ('Description:' + $('.detailsDescription').text());
+					//console.log ('Schedule:' + $('#but1 a:contains()').attr('href'));
+					//console.log ('Image:' +$('.ad-thumbs ul').html());
+					//console.log ('Image2:' + $('.ad-thumbs ul').attr('img src','href'));
+					//console.log ('Image3:' + $('.ad-thumbs ul').html($("<img src>").attr("href")));
+
+					//links = $('.ad-thumbs a');
+					//$(links).each(function (i,link) {
+					//	console.log($(link).text() + '\n ' + $(link).attr('href'));
+					//});
+
+				});
+			});
+		}); 
+	}
