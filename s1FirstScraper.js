f334cb4c2e6e2600e6739cc7adf34b3049186767
AliMillican1 <alastair.millican@hotmail.co.uk>
2013-08-03 10:56:13 +0100

    working rabbit messaging

--- s1FirstScraper.js
+++ s1FirstScraper.js
@@ -6,49 +6,45 @@
 	var amqp = require('amqp');	//used for messaging
 
 
+	function pub() {
+		var exchange = conn.exchange(''); // get the default exchange
+  		var queue = conn.queue('trial', {}, function() { // create a queue		
 
-	for (counter=1;counter<3;counter++) {
-		var url = 'http://www.s1homes.com/property-for-sale/forsale_search_results.cgi?type=House&newhomes=yes&sort=da&page=' +counter;
+			for (counter=1;counter<3;counter++) {
+				var url = 'http://www.s1homes.com/property-for-sale/forsale_search_results.cgi?type=House&newhomes=yes&sort=da&page=' +counter;
 
-		request(url, {proxy: 'http://localhost:8888'}, function(err, resp, body) {
+				request(url, function(err, resp, body) {
 
-			if (err)
+					if (err)
 
-				throw err;
+						throw err;
 
-			$ = cheerio.load(body);
+					$ = cheerio.load(body);
 
-			$('.imgPlaceholder a:contains()').each(function() {
+					$('.imgPlaceholder a:contains()').each(function() {
 
-				console.log ('http://www.s1homes.com/' + $(this).attr('href'));
+						//console.log ('http://www.s1homes.com/' + $(this).attr('href'));
 
-				var url2 = 'http://www.s1homes.com/' + $(this).attr('href');
-				console.log (url2)
+						var url2 = 'http://www.s1homes.com/' + $(this).attr('href');
+						//console.log (url2)
 
-				
-			});
-		}); 
-	}
+						exchange.publish(queue.name, {body: url2});	//publish message containing urls
+	   					console.log("published");
+
+						
+					});
+				}); 
+			}
+		});	
+	};
+
+	var url = process.env.CLOUDAMQP_URL || "amqp://gnmehswn:IlbEqsWPcK3tYO6S_lIJexWu4TxWMtce@bunny.cloudamqp.com/gnmehswn"; //connect to host
+	var implOpts = {
+	reconnect: true,
+	reconnectBackoffStrategy: 'linear', // or 'exponential'
+	reconnectBackoffTime: 500, // ms
+	};
+	var conn = amqp.createConnection({ url: url }, implOpts); // create the connection
+	conn.on('ready', pub); // when connected, call "pub"
 
 
-/*
-function pub() {
-  var exchange = conn.exchange(''); // get the default exchange
-  var queue = conn.queue('urls', {}, function() { // create a queue
-    
-    // publish a message
-    exchange.publish(queue.name, {body: 'Hello CloudAMQP!'});
-    console.log("published");
-  });
-}
-
-var url = process.env.CLOUDAMQP_URL || "amqp://gnmehswn:IlbEqsWPcK3tYO6S_lIJexWu4TxWMtce@bunny.cloudamqp.com/gnmehswn"; // default to localhost
-var implOpts = {
-  reconnect: true,
-  reconnectBackoffStrategy: 'linear', // or 'exponential'
-  reconnectBackoffTime: 500, // ms
-};
-var conn = amqp.createConnection({ url: url }, implOpts); // create the connection
-conn.on('ready', pub); // when connected, call "pub_and_sub"
-
-*/
--- s2SecondScraper.js
+++ s2SecondScraper.js
@@ -2,18 +2,63 @@
 
 	var cheerio = require('cheerio'); // cheerio mimics the DOM and jQuery/CSS style selectors
 
+	var amqp = require('amqp');	//used for messaging
 
-	request (/* load url from queue here*/), function(err,resp,body) {
-		
-		$ = cheerio.load(body);
-		
-		console.log ('Address:' + $('.detailsKeyPropertyDetails h1').text());
-		console.log ('Rooms:' + $('.detailsKeyPropertyDetails h2').text());
-		console.log ('Description:' + $('.detailsDescription').text());
-		console.log ('Schedule:' + $('#but1 a:contains()').attr('href'));
-		console.log('Images:');
-		links = $('.ad-thumbs a');
-		$(links).each(function (i,link) {
-			console.log($(link).text() + '\n ' + $(link).attr('href'));
-		});
-	});
+
+	function sub() {
+  		var exchange = conn.exchange(''); // get the default exchange
+  		var queue = conn.queue('trial', {}, function() { // create a queue	
+  			queue.subscribe(function(msg) { // subscribe to that queue
+	      		//console.log(msg.body); // print new messages to the console
+	      		url3 = (msg.body)
+	      		//console.log(url3)
+
+				request (url3, function(err,resp,body) {
+					console.log(url3)
+					$ = cheerio.load(body);
+					
+					console.log ('Address:' + $('.detailsKeyPropertyDetails h1').text());
+					console.log ('Rooms:' + $('.detailsKeyPropertyDetails h2').text());
+					console.log ('Description:' + $('.detailsDescription').text());
+					console.log ('Schedule:' + $('#but1 a:contains()').attr('href'));
+					console.log('Images:');
+					links = $('.ad-thumbs a');
+					$(links).each(function (i,link) {
+						console.log($(link).text() + '\n ' + $(link).attr('href'));
+					});
+				});
+  			});
+  		});
+	}
+
+
+	var url = process.env.CLOUDAMQP_URL || "amqp://gnmehswn:IlbEqsWPcK3tYO6S_lIJexWu4TxWMtce@bunny.cloudamqp.com/gnmehswn"; // default to localhost
+	var implOpts = {
+	  reconnect: true,
+	  reconnectBackoffStrategy: 'linear', // or 'exponential'
+	  reconnectBackoffTime: 500, // ms
+	};
+	var conn = amqp.createConnection({ url: url }, implOpts); // create the connection
+	conn.on('ready', sub); // when connected, call "pub_and_sub"
+
+/*
+function sub() {
+  var exchange = conn.exchange(''); // get the default exchange
+  var queue = conn.queue('test', {}, function() { // create a queue
+    
+    queue.subscribe(function(msg) { // subscribe to that queue
+      console.log("subscribe" + msg.body); // print new messages to the console
+    });
+    
+  });
+}
+
+var url = process.env.CLOUDAMQP_URL || "amqp://gnmehswn:IlbEqsWPcK3tYO6S_lIJexWu4TxWMtce@bunny.cloudamqp.com/gnmehswn"; // default to localhost
+var implOpts = {
+  reconnect: true,
+  reconnectBackoffStrategy: 'linear', // or 'exponential'
+  reconnectBackoffTime: 500, // ms
+};
+var conn = amqp.createConnection({ url: url }, implOpts); // create the connection
+conn.on('ready', sub); // when connected, call "pub_and_sub"
+*/
